7f486128b28b42c3c5acfd433194f8b17948400d
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-08-15 22:23:12 +0530

    Time: 3 ms (14.10%) | Memory: 47 MB (57.13%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -14,7 +14,7 @@ class Solution {
             }
         }
         result.add(newInterval);
-        Collections.sort(result, (a, b) -> Integer.compare(a[0], b[0]));
+        Collections.sort(result, (a, b) -> a[0] - b[0]);
 
         return result.toArray(new int[result.size()][]);
     }
