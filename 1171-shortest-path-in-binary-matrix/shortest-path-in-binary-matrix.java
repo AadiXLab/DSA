e6e7d4a426646ddb6a4897f7ca639c075a5b9ffa
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-08-17 22:31:26 +0530

    Time: 13 ms (82.14%) | Memory: 48.2 MB (6.49%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,19 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
 
-        if (grid[0][0] == 1) {
-            return -1;
-        }
-
         int m = grid.length;
         int n = grid[0].length;
+
+        if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0) {
+            return -1;
+        }
 
         Queue<int[]> q = new LinkedList<>();
 
         // {row, column, distance}
         q.add(new int[]{0, 0, 1});
 
-        // Mark starting point visited
+        // Mark visited
         grid[0][0] = 1;
 
         int[][] dir = {
@@ -29,35 +31,32 @@ class Solution {
 
         while (!q.isEmpty()) {
 
-            int size = q.size();
+            int[] point = q.poll();
 
-            while (size-- > 0) {
+            int row = point[0];
+            int col = point[1];
+            int distance = point[2];
 
-                int[] point = q.poll();
+            // Destination reached
+            if (row == m - 1 && col == n - 1) {
+                return distance;
+            }
 
-                if (point[0] == m - 1 && point[1] == n - 1) {
-                    return point[2];
-                }
+            // Explore 8 directions
+            for (int[] d : dir) {
 
-                for (int[] d : dir) {
+                int r = row + d[0];
+                int c = col + d[1];
 
-                    int r = point[0] + d[0];
-                    int c = point[1] + d[1];
+                if (r >= 0 && r < m &&
+                    c >= 0 && c < n &&
+                    grid[r][c] == 0) {
 
-                    // Valid and unvisited
-                    if (r >= 0 && r < m &&
-                        c >= 0 && c < n &&
-                        grid[r][c] == 0) {
+                    // Mark visited
+                    grid[r][c] = 1;
 
-                        q.add(new int[]{
-                            r,
-                            c,
-                            point[2] + 1
-                        });
-
-                        // Mark visited
-                        grid[r][c] = 1;
-                    }
+                    // Add next cell with distance + 1
+                    q.add(new int[]{r, c, distance + 1});
                 }
             }
         }
