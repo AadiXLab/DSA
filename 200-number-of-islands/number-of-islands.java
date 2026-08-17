dac9f88b6cb1b4e98c9af391c85052558d13db3f
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-08-17 20:45:37 +0530

    Time: 3 ms (87.62%) | Memory: 52.4 MB (46.02%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+       
+        int count =0;
+
+        if(grid == null || grid.length ==0 || grid[0].length ==0){
+            return 0;
+        }
+
+        for(int i =0; i<grid.length; i++){
+            for(int j =0; j< grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;  
+    }
+    // this function is used to explaore more land wala cell kyuki woh sb 1 island consider honge
+    private void dfs(char[][] grid, int i, int j){
+        
+        if(i<0 || j<0|| i>=grid.length || j>=grid[0].length ||grid[i][j] == '0'){
+            return;
+        }
+        // mark the cell as visited
+        grid[i][j] = '0';
+
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+    }
+}
