565762b0dade6a928acef7d98730d17265802281
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-08-29 00:21:21 +0530

    Time: 16 ms (38.75%) | Memory: 47.7 MB (33.63%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,48 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
+        // Count frequencies
         HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
 
-        // Frequency map
-        for (int i = 0; i < nums.length; i++) {
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
 
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
+        // Min-heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
+        );
+
+        // Keep only k elements
+        for (int num : map.keySet()) {
+
+            pq.add(num);
+
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
 
-        // Find maximum k times
-        for (int i = 0; i < k; i++) {
-
-            int max = Integer.MIN_VALUE;
-            int maxNum = 0;
-
-            // Find maximum frequency
-            for (int num : map.keySet()) {
-
-                if (map.get(num) > max) {
-                    max = map.get(num);
-                    maxNum = num;
-                }
-            }
-
-            // Store answer
-            list.add(maxNum);
-
-            // Remove it so we don't choose it again
-            map.remove(maxNum);
-        }
-
-        // Convert ArrayList<Integer> → int[]
-        int[] ans = new int[k];
+        // Put heap elements into answer
+        int[] answer = new int[k];
 
         for (int i = 0; i < k; i++) {
-            ans[i] = list.get(i);
+            answer[i] = pq.poll();
         }
 
-        return ans;
+        return answer;
     }
 }
