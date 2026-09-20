97b3638be6554317d8fcde42c4e1afcae3bc0d81
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-09-21 00:02:18 +0530

    Time: 20 ms (65.54%) | Memory: 45.8 MB (46.71%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -33,16 +33,13 @@ class Solution {
                         result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
 
                         // Skip duplicates
-                        while (left < right &&
-                               nums[left] == nums[left + 1]) {
+                        while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
 
-                        while (left < right &&
-                               nums[right] == nums[right - 1]) {
+                        while (left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
-
                         left++;
                         right--;
                     }
