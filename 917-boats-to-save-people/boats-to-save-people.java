23def9e030fdf5de1cc3136c2630043586da249f
Aditya Jaiswal <adityarnktwork@gmail.com>
2026-09-21 11:05:38 +0530

    Time: 20 ms (81.89%) | Memory: 55.9 MB (98.86%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -8,6 +8,7 @@ class Solution {
         int right = people.length - 1;
 
         while (left <= right) {
+            // repetion ka darr ai kyuki Q bolta hai ki kisi bhi person ka weight limit se zayda ni hoga
 
             if (people[left] + people[right] <= limit) {
                 left++;
